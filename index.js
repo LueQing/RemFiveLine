29f40a6843a1ee6990644991a12c18af927beb24
JensonYang2020 <jensonyang2020@outlook.com>
2020-09-09 20:45:18 +0800

    ok

--- index.js
+++ index.js
@@ -16,14 +16,26 @@ let FiveLine = {
 
 let FiveLineQuestions = ["下加3间","下加2线","下加1线","下加1间","1线","1间","2线","2间","3线","3间","4线","4间","5线"];
 
-let now = 20;
+let now = "";
+let nowTime = "";
+let second = 0;
+let minute = 0;
 
-if (localStorage.getItem('total_right') == null) {
-    localStorage.setItem('total_right', '0');
-    localStorage.setItem('total_wrong', '0');
+function total_questions_add(num) {
+    let total_right = Number(localStorage.getItem("total_right"));
+    let total_wrong = Number(localStorage.getItem("total_wrong"));
+    if (num > 0) {
+        total_right += num;
+        localStorage.setItem('total_right', total_right);
+    } else {
+        total_wrong -= num;
+        localStorage.setItem('total_wrong', total_wrong);
+    }
+}
 
-    localStorage.setItem('total_minute', '0');
-    localStorage.setItem('total_second', '0');
+function update_total_questions() {
+    document.getElementById('total_right').innerHTML = "你一共对了: " + Number(localStorage.getItem("total_right")) + " 题";
+    document.getElementById('total_wrong').innerHTML = "你一共错了: " + Number(localStorage.getItem("total_wrong")) + " 题";
 }
 
 function SendQuestion() {
@@ -32,10 +44,69 @@ function SendQuestion() {
         SendQuestion();
     } else {
         document.getElementById("question").innerHTML = "请问" + FiveLineQuestions[randint] + "发的是什么音";
-        now = randint;
+        now = FiveLineQuestions[randint];
     }
+    int = setInterval(timer,1000);
 }
 
 function SendAnswer() {
+    window.clearInterval(int);
+    total_time_add()
+    update_total_times();
+    let Answer = document.getElementById("answer");
+    if (Answer.value == FiveLine[now]) {
+        total_questions_add(1);
+        document.getElementById("last").innerHTML = "上一题是: 请问" + now + "发的是什么音"+";你答对了"
+    } else {
+        total_questions_add(-1);
+        document.getElementById("last").innerHTML = "上一题是: 请问" + now + "发的是什么音"+";你答错了,正确答案为:  " + FiveLine[now]
+    }
 
+
+    Answer.value = "";
+    update_total_questions();
+    SendQuestion();
 }
+
+function timer() {
+    second += 1;
+    if (second >= 60) {
+      second = 0;
+      minute = minute + 1;
+    }
+    nowTime = minute+'分'+second+'秒';
+    document.getElementById("lasttime").innerHTML = "对这道题已经耗时:" + nowTime;
+}
+
+function total_time_add() {
+    let total_minute = Number(localStorage.getItem("total_minute"));
+    let total_second = Number(localStorage.getItem("total_second"));
+    total_minute += minute;
+    total_second += second;
+    if (total_second >= 60) {
+        total_minute += 1;
+        total_second -= 60;
+    }    
+    localStorage.setItem('total_minute', total_minute);
+    localStorage.setItem('total_second', total_second);
+}
+function update_total_times() {
+    let total_minute = Number(localStorage.getItem("total_minute"));
+    let total_second = Number(localStorage.getItem("total_second"));
+    document.getElementById("total_time").innerHTML = "你一共花了:  " + total_minute + "分" + total_second + "秒";
+}
+
+function init() {
+    if (localStorage.getItem('total_right') == null) {
+        localStorage.setItem('total_right', '0');
+        localStorage.setItem('total_wrong', '0');
+    
+        localStorage.setItem('total_minute', '0');
+        localStorage.setItem('total_second', '0');
+    }
+
+    update_total_questions();
+    update_total_times();
+    SendQuestion();
+}
+
